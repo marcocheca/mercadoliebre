8e875dbac607728ca6131a2583116265753c9ee9
Marco Checa <checa.marco2002@gmail.com>
2021-09-16 22:10:53 -0300

    modifique el archivo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(morgan('tiny'));
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=> console.log('servidor corriendo en http://localhost:3000'));
+app.listen(process.env.PORT || 3000, ()=> console.log('servidor corriendo en http://localhost:3000'));
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'views/home.html'))
